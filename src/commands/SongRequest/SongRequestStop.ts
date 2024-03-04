2559f91aa4fdbeca55e2131ed15a9c2a1035c444
fvlvte <me@falat.me>
2024-03-04 15:57:55 -0700

    feat(be-1): SR play/stop backend logic

---
+++ src/commands/SongRequest/SongRequestStop.ts
@@ -0,0 +1,36 @@
+import { CommandHandler } from "../CommandHandler";
+import { TwitchMessage } from "../../types/TwitchTypes";
+import { TranslationManager } from "../../managers/TranslationManager";
+import { Songrequest } from "../../features/Songrequest";
+import { TwitchClient } from "../../clients/TwitchClient";
+
+export class SongRequestStop extends CommandHandler {
+  async handleCommand(
+    client: TwitchClient,
+    message: TwitchMessage,
+  ): Promise<void> {
+    const translationManager = TranslationManager.getInstance(
+      await client.getStreamLanguage(),
+      await client.getStreamerUsername(),
+    );
+
+    const sr = Songrequest.getInstance(await client.getBroadcasterId());
+
+    if (
+      message.tags.isModerator ||
+      message.username.toLowerCase() ===
+        (await client.getStreamerUsername()).toLowerCase()
+    ) {
+      await client.dispatchBotMessage(
+        translationManager.translate("SR_ADMIN_STOP", {
+          invokedBy: message.username,
+        }),
+      );
+      sr.stop();
+    }
+  }
+
+  getMatchingExp(): RegExp {
+    return /^!(stop)|(pause)\s*$/i;
+  }
+}
